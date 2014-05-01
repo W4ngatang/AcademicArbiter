5b78da31b391ecf1e8ce8e87179c80638dbf0bec
Amanpreet Kandola <amanpreet123@gmail.com>
2014-05-01 00:14:51 -0400

    updated ui

--- academicarbiter/course.py
+++ academicarbiter/course.py
@@ -1,5 +1,4 @@
 class course:
-    concentration = ""
     #scores are either 1 or -1, its just thumbs up or down
     #difficulty, teaching, subject matter
     dScore = 0
@@ -8,26 +7,22 @@ class course:
     name = ""
     
     
-    def __init__ (self, name, concentration, dscore, 
+    def __init__ (self, name, dscore, 
                   tscore, sscore):
         self.name = name
         self.dScore = dscore
         self.tScore = tscore
         self.sScore = sscore
-        self.concentration = concentration
     
     def getName (self):
         return self.name
-    def getScore(self):
-        return self.qscore
     def getT(self):
         return self.tScore
     def getD(self):
         return self.dScore
     def getS(self):
         return self.sScore
-    def getConcentration(self):
-        return self.concentration
+
     
     
     
